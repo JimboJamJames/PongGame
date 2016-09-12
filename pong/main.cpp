f70e1621bff4ddb73fe387e070a99bc23fc4749b
Sergeijyle <James.lyle@students.aie.edu.au>
2016-09-12 09:45:42 -0700

    updated Game State

--- pong/main.cpp
+++ pong/main.cpp
@@ -3,60 +3,71 @@
 #include "Collisions.h"
 #include "Struct.h"
 #include "GameState.h"
+
+
 void main()
 {
-	Ball circle;
-	Paddle1 RightPaddle;
-	Paddle2 LeftPaddle;
+
 	sfw::initContext(800, 600, "SFW Draw");
-	float acc = 0;
-	char c = '\0';
+	//Ball circle;
+	//Paddle1 RightPaddle;
+	//Paddle2 LeftPaddle;
 
-	
-	bool isXPositive = false, isYPositive = true;
+	//float acc = 0;
+	//char c = '\0';
+
+	//
+	//bool isXPositive = false, isYPositive = true;
 
 	sfw::setBackgroundColor(BLACK);
 
-	
-	while (sfw::stepContext())
-	{
-			Collision(circle);
-			Collision2(circle, LeftPaddle);
-			Collision3(circle, RightPaddle);
-			if (sfw::getKey('W')&&LeftPaddle.y < 400)
-				LeftPaddle.y += sfw::getDeltaTime() * 900;
-			
-			if (sfw::getKey('S') && LeftPaddle.y > 0)
-				LeftPaddle.y -= sfw::getDeltaTime() * 900;
-
-			sfw::drawLine(LeftPaddle.x, LeftPaddle.y, LeftPaddle.x, LeftPaddle.y + LeftPaddle.size, RED);
-
-			if (sfw::getKey('I') && RightPaddle.y < 400)
-				RightPaddle.y += sfw::getDeltaTime() * 600;
-
-			if (sfw::getKey('K') && RightPaddle.y > 0)
-				RightPaddle.y -= sfw::getDeltaTime() * 600;
-
-			sfw::drawLine(RightPaddle.x, RightPaddle.y, RightPaddle.x, RightPaddle.y + RightPaddle.size, RED);
-			
-			circle.ballX = circle.ballX + circle.momentumX;
-			circle.ballY = circle.ballY + circle.momentumY;
-	
-			sfw::drawCircle(circle.ballX, circle.ballY, 10, 20, CYAN);
-	}
-sfw::termContext();
-}
-void gameState()
-{
-	sfw::initContext(800, 600, "NSFW Draw");
-	sfw::setBackgroundColor(BLACK);
-
-	GameState gs = createGameState();
+	GameState gs;
 
 	while (sfw::stepContext())
 	{
 		updateGameState(gs);
 		drawGameState(gs);
+			//Collision(circle);
+			//Collision2(circle, LeftPaddle);
+			//Collision3(circle, RightPaddle);
+			//if (sfw::getKey('W')&&LeftPaddle.y < 400)
+			//	LeftPaddle.y += sfw::getDeltaTime() * 900;
+			//
+			//if (sfw::getKey('S') && LeftPaddle.y > 0)
+			//	LeftPaddle.y -= sfw::getDeltaTime() * 900;
+
+
+			//if (sfw::getKey('I') && RightPaddle.y < 400)
+			//	RightPaddle.y += sfw::getDeltaTime() * 600;
+
+			//if (sfw::getKey('K') && RightPaddle.y > 0)
+			//	RightPaddle.y -= sfw::getDeltaTime() * 600;
+
+			//
+			//circle.ballX = circle.ballX + circle.momentumX;
+			//circle.ballY = circle.ballY + circle.momentumY;
+
+			//sfw::drawLine(LeftPaddle.x, LeftPaddle.y, LeftPaddle.x, LeftPaddle.y + LeftPaddle.size, RED);
+
+			//sfw::drawLine(RightPaddle.x, RightPaddle.y, RightPaddle.x, RightPaddle.y + RightPaddle.size, RED);
+
+			//sfw::drawCircle(circle.ballX, circle.ballY, 10, 20, CYAN);
 	}
-	sfw::termContext();
+sfw::termContext();
 }
+
+
+//void gameState()
+//{
+//	sfw::initContext(800, 600, "NSFW Draw");
+//	sfw::setBackgroundColor(BLACK);
+//
+//	GameState gs = createGameState();
+//
+//	while (sfw::stepContext())
+//	{
+//		updateGameState(gs);
+//		drawGameState(gs);
+//	}
+//	sfw::termContext();
+//}
